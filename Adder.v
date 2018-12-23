8d116ef7e04b32f4717ba5273f7f554932286d4e
MDhamedAH <45857576+MDhamedAH@users.noreply.github.com>
2018-12-23 09:21:51 +0330

    Update Adder.v

--- Adder.v
+++ Adder.v
@@ -1,6 +1,6 @@
 module adder(DataIn1 , DataIn2 , DataOut);
 input [63:0]DataIn1;
 input [63:0]DataIn2;
-output[63:0] DataOut;
-	assign result = data1+data2;
+output[63:0]DataOut;
+	assign DataOut = DataIn1+DataIn2;
 endmodule
