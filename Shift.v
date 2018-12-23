9c4a046b008b31b3cf57b2c4c28c66bc7b8ce466
MDhamedAH <45857576+MDhamedAH@users.noreply.github.com>
2018-12-23 08:39:46 +0330

    Update Shift.v

--- Shift.v
+++ Shift.v
@@ -1,6 +1,6 @@
 module Shift(DataIn , DataOut);
-  input [63:0]DataIn;
-  output[63:0] DataOut;
-  wire [1:0] X = 2'b0;
-	assign DataOut = {DataIn[63:0] , X};
+	input [63:0] DataIn;
+        output [63:0] DataOut;
+
+	    assign DataOut = DataIn << 2;
 endmodule
