29d8f058ad06039eaaea0803120ea4a3331f5b9e
Mohammad Hamed Ahmadinia <45857576+MDhamedAH@users.noreply.github.com>
2018-12-30 09:32:32 +0330

    Update SignExtend.v

--- SignExtend.v
+++ SignExtend.v
@@ -1,5 +1,14 @@
-module SignExtend(DataIn , DataOut);
-	input [31:0] DataIn;
-	output [63:0] DataOut;
-	assign DataOut = {{32{DataIn[31]}},DataIn[31:0]};
+module SignExtend(instruction, output_data);
+
+	input [31:0] instruction;
+	output reg [63:0] output_data;
+	wire opcode = instruction[30];
+
+	always @ (instruction) begin
+		case(opcode)
+			2'b1: output_data = {{55{instruction[20]}}, instruction[20:12]};
+			2'b0: output_data = {{45{instruction[23]}}, instruction[23:5]};
+		endcase
+	end
+
 endmodule
